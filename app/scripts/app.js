708cd8cf8ca1c251eb53b99e2f81fe552620dc00
AnithaPal <anitha.aravindvel@gmail.com>
2016-01-26 09:21:33 -0500

    task controller state added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,21 @@
-angular.module('pomodoro', []);
+(function(){
+	function config($stateProvider, $locationProvider){
+		$locationProvider
+			.html5Mode({
+			enabled: true,
+			requireBase: false
+		});
+		$stateProvider
+			.state('taskTimer', {
+				url: '/',
+				controller: 'TaskTimerCtrl as taskTimer',
+				templateUrl: 'templates/task_timer.html'	
+			});
+			
+		
+	}
+	
+	angular
+		.module('pomodoro', ['ui.router'])
+		.config(config);	
+})();
