e1a083b86ecc82581fcbc0b28cb474fd552d0c3b
AnithaPal <anitha.aravindvel@gmail.com>
2016-01-27 14:48:21 -0500

    break time feature added

--- app/scripts/directives/timer.js
+++ app/scripts/directives/timer.js
@@ -5,38 +5,91 @@
 			 templateUrl: '/templates/directives/timer.html',
 			 replace: true,
 			 restrict: 'E',
-			 scope: {},
+			 scope: {
+			 },
 			 link: function(scope, element, attributes){
-				 
 				/**
- 					* @desc to store instance of $interval service
+ 					* @desc to store instance of $interval service for timer
  					* @type {Object}
  				*/
 				var interval;
+				 
+				 /**
+ 					* @desc to store instance of $interval service for break
+ 					* @type {Object}
+ 				*/
+				var breakInterval; 
+			
+				 /**
+ 					* @desc to store counter value
+ 					* @type {Object}
+ 				*/ 
+				 var counter = 0;
 				/**
 					 * @function decrementTime
 					 * @desc decrement currentTime after 1000ms from $interval service
 				*/
 				var decrementTime = function(){
 					if(scope.currentTime > 0){
-					scope.currentTime -= 1;
-					}
-					else{
+						counter +=1;
+						console.log("current time counter: "+ counter);
+						scope.currentTime -= 1;
+						if(counter === 1500){
+							scope.onBreak = true;
+							scope.stop();
+						} 
+					}else {
 						scope.stop();
 					}
 				};
 				 
+				 var decrementBreakTime = function(){
+					if(scope.currentBreakTime > 0){
+						console.log
+						counter +=1;
+						console.log('break time counter : ' + counter);
+						scope.currentBreakTime -= 1;
+						if(counter === 300){
+							scope.isTimerActive = false;
+							counter = 0;
+							scope.breakStop();	
+						} 
+					}
+					else {
+						scope.breakStop();
+						scope.isTimerActive = false;
+					}
+				};
+				 
+//				attributes.$observe('totaltime', function(totalTime) {
+//					
+//				});
+				
+			
 				/**
  					* @desc to store current time.
  					* @type {Object}
  				*/
-				scope.currentTime = 1500;
+				scope.currentTime = attributes.totaltime;
 				 
-				 /**
- 					* @desc to store status of a button
+				/**
+ 					* @desc to store break time.
  					* @type {Object}
  				*/
-				scope.isActive = false;
+				scope.currentBreakTime = attributes.breaktime;
+				
+				 
+				 /**
+ 					* @desc to store status of a timer wheather to display  start or reset button.
+ 					* @type {Object}
+ 				*/
+				scope.isTimerActive = false;
+				 
+				 /**
+ 					* @desc to store status of break button 
+ 					* @type {Object}
+ 				*/
+				 scope.onBreak = false;
 				 
 				 /**
 					 * @function start
@@ -44,7 +97,7 @@
 
  				*/
 				scope.start = function(){
-					scope.isActive = true; 
+					scope.isTimerActive = true; 
 					interval = $interval(decrementTime, 1000);
 					
 				};
@@ -55,8 +108,8 @@
  				*/
 				 scope.stop = function(){
 					 $interval.cancel(interval);
-					 scope.currentTime = 1500;
-					 scope.isActive = false; 
+					 scope.currentTime = attributes.totaltime;
+					 scope.isTimerActive = false; 
 				 }
 				 
 				 /**
@@ -64,8 +117,30 @@
 					 * @desc resets the timer
  				*/
 				 scope.reset = function(){
-					 scope.currentTime = 1500;
+					 scope.currentTime = attributes.totaltime;
 				 
+				 }
+				 
+				 /**
+					 * @function break
+					 * @desc timer for breaks
+ 				*/
+				 
+				 scope.break = function(){
+					 scope.stop();
+					 counter = 0;
+					 breakInterval = $interval(decrementBreakTime, 1000);
+					 
+				 }
+				 
+				 /**
+					 * @function breakstop
+					 * @desc stops the timer
+ 				*/
+				 scope.breakStop = function(){
+					  $interval.cancel(breakInterval);
+					  scope.currentBreakTime = attributes.breaktime;
+					  scope.onBreak = false;
 				 }
 				
 			}	
