6ddba870ba2942861522f16609b1b011a3215ef4
AnithaPal <anitha.aravindvel@gmail.com>
2016-02-02 12:15:31 -0500

    Task service used in task timer controller and view

--- app/scripts/controllers/TaskTimerCtrl.js
+++ app/scripts/controllers/TaskTimerCtrl.js
@@ -1,13 +1,28 @@
 (function(){
-	function TaskTimerCtrl(){
+	function TaskTimerCtrl(TaskService, $scope){
+		
 		this.taskTime = 1500;
 		this.breakTime = 300;
 		this.longBreakTime = 1800;
+		
+		this.tasks = TaskService.all();
+		console.log(this.tasks);
+		
+		this.addTask = function(task) {
+			TaskService.create(task);
+			$scope.taskName = null;
+		};
+		
+		this.deleteTask = function(task) {
+			TaskService.delete(task);
+		}
 	};
+	
+	
 	
 	angular
 		.module('pomodoro')
-		.controller('TaskTimerCtrl', [TaskTimerCtrl])
+		.controller('TaskTimerCtrl', ['TaskService', '$scope',TaskTimerCtrl])
 	
 })();
 
