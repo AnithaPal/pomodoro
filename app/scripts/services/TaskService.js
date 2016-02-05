3dad8b9c300168ec97bc006c914b9c4230efbc7b
AnithaPal <anitha.aravindvel@gmail.com>
2016-02-05 06:30:13 -0500

    timestamp property added to task object and order based on timestamp

--- app/scripts/services/TaskService.js
+++ app/scripts/services/TaskService.js
@@ -1,27 +1,30 @@
 (function(){
 	function TaskService($firebaseArray){
 		var TaskService = {};
-		
+
 		var firebaseref = new Firebase("https://pomodoro-tasktimer.firebaseio.com/");
+
+
 		var taskList = $firebaseArray(firebaseref);
-		
+
 		TaskService.create = function(taskName){
-			taskList.$add({ taskName: taskName });
+			taskList.$add({ taskName: taskName ,
+										created_at: Firebase.ServerValue.TIMESTAMP});
 		};
-					  
+
 		TaskService.delete = function(taskID){
 			taskList.$remove(taskID);
 		};
-		
+
 		TaskService.all = function() {
 			return taskList;
 		};
-		
+
 		return TaskService;
 	}
-	
+
 	angular
 	.module("pomodoro")
 	.factory('TaskService', ['$firebaseArray', TaskService]);
-	
+
 })();
