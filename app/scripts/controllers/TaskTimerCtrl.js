e0f993edbc1fa4058fda645df14f7647be55f1d8
AnithaPal <anitha.aravindvel@gmail.com>
2016-02-05 15:54:51 -0500

    sessionQty property and newly created task can be displayed on the timer window

--- app/scripts/controllers/TaskTimerCtrl.js
+++ app/scripts/controllers/TaskTimerCtrl.js
@@ -8,21 +8,37 @@
 		this.tasks = TaskService.all();
 		console.log(this.tasks);
 
-		this.addTask = function(task) {
 
+		// sets created task as a default task
+		TaskService.getLastAdded(function (snapshot) {
+      snapshot.forEach(function (task) {
+        $scope.lastAddedTask = task.val().name;
+				// console.log($scope.lastAddedTask);
+      });
+    });
+
+ console.log($scope.lastAddedTask);
+
+
+
+		this.addTask = function(task) {
 			TaskService.create(task);
-			$scope.taskName = null;
+			$scope.task = null;
 		};
 
 		this.deleteTask = function(task) {
 			TaskService.delete(task);
 		}
+		this.setAsCurrent = function(task){
+			this.currentTask = task;
+			console.log('I am from setAsCurrent' + this.currentTask.name);
+		};
+
+
 	};
-
-
 
 	angular
 		.module('pomodoro')
-		.controller('TaskTimerCtrl', ['TaskService', '$scope',TaskTimerCtrl])
+		.controller('TaskTimerCtrl', ['TaskService', '$scope', TaskTimerCtrl])
 
 })();
--- app/scripts/services/TaskService.js
+++ app/scripts/services/TaskService.js
@@ -1,24 +1,37 @@
 (function(){
 	function TaskService($firebaseArray){
 		var TaskService = {};
-
 		var firebaseref = new Firebase("https://pomodoro-tasktimer.firebaseio.com/");
-
-
 		var taskList = $firebaseArray(firebaseref);
 
-		TaskService.create = function(taskName){
-			taskList.$add({ taskName: taskName ,
-										created_at: Firebase.ServerValue.TIMESTAMP});
+		TaskService.create = function(task){
+			taskList.$add({ name: task.name,
+											sessionQty: task.sessionQty,
+										  created_at: Firebase.ServerValue.TIMESTAMP});
 		};
 
 		TaskService.delete = function(taskID){
 			taskList.$remove(taskID);
 		};
 
+		TaskService.getLastAdded = function (callback) {
+        firebaseref.orderByChild("name").limitToLast(1).on("value", callback);
+      }
+
 		TaskService.all = function() {
 			return taskList;
 		};
+
+		// TaskService.setLast = function(){
+		//
+		// 	firebaseref.orderByChild("id").limitToLast(1).on("value", function (snapshot) {
+		// 		$scope.last = snapshot.val();
+		// 	})
+		//
+		// }
+
+
+
 
 		return TaskService;
 	}
@@ -28,3 +41,13 @@
 	.factory('TaskService', ['$firebaseArray', TaskService]);
 
 })();
+
+
+//  TaskService.addWorkSession = function(task, workSession){
+// 	var index = taskList.$indexOf( task.$id );
+// 	console.log(index);
+// 	var item = taskList[task.$id]
+// 	item.no_worksession = workSession;
+// 	taskList.$set(item.$id, item);
+// 	// task.update({no_worksession: workSession})
+// };
